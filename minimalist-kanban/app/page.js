62925ab9adaafeaf61a95fb7c13b9e432f1b9a5b
danielhong <dan.hong378@gmail.com>
2024-02-21 23:24:39 +1100

    Fixed an issue preventing compile

--- minimalist-kanban/app/page.js
+++ minimalist-kanban/app/page.js
@@ -4,8 +4,6 @@ import item from "./scripts/item"
 import category from "./scripts/category";
 import SingletonStorageManager from "./scripts/boardSingleton";
 import { useState, useEffect } from "react";
-import Category from "./components/Category";
-
 
 // Add keyboard shortcuts to make this even better
 // Add colour/theme selector
