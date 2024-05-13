134750792cf932949c060f3c2c1f89424f5e3a21
suspiciouscap <108613049+suspiciouscap@users.noreply.github.com>
2024-05-13 22:42:26 +1000

    Added auth

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,3 +1,4 @@
+import { ClerkProvider, SignInButton, SignedOut } from '@clerk/nextjs'
 import './globals.css'
 import { inter } from './styles/fonts'
 
@@ -8,13 +9,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} h-screen w-screen flex flex-col`}>
-        <nav className='flex flex-col'>
-          
-        </nav>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} h-screen w-screen flex flex-col`}>
+          <nav className='flex flex-col'>
+            <SignedOut>
+              <SignInButton/>
+            </SignedOut>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+
   )
 }
