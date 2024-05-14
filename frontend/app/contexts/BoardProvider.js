36c60f1a8fb9561b721c0f2428ed82eb85a4549b
danielhong <dan.hong378@gmail.com>
2024-05-14 16:57:00 +1000

    Final commit before implementing auth - fixed some UI

--- frontend/app/contexts/BoardProvider.js
+++ frontend/app/contexts/BoardProvider.js
@@ -1,5 +1,5 @@
+"use client";
 import React, { createContext, useState, useContext, useEffect } from 'react';
-
 
 const BoardDataContext = createContext()
 
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,4 +1,4 @@
-import { ClerkProvider, SignInButton, SignedOut } from '@clerk/nextjs'
+
 import './globals.css'
 import { inter } from './styles/fonts'
 
@@ -9,18 +9,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    
       <html lang="en">
         <body className={`${inter.className} h-screen w-screen flex flex-col`}>
-          <nav className='flex flex-col'>
-            <SignedOut>
-              <SignInButton/>
-            </SignedOut>
+          <nav className='flex flex-row justify-end p-4 h-16 items-center'>
+            
           </nav>
           {children}
         </body>
       </html>
-    </ClerkProvider>
+ 
 
   )
 }
